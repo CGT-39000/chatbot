330028770765d2cc0e781a153f2aa35d1b777beb
xM4ddy <59680197+xM4ddy@users.noreply.github.com>
2025-04-06 23:50:40 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="chatbox">
+        <div className="mcontainer">
+          <div className="message l">
+            <p>Hi</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message r">
+            <p>Hello, How Are You?</p>
+          </div>
+          <div className="message l">
+            <p>Hi</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <input type="text" class="messageInput" />
+      </div>
+    </>
+  );
+}
+
+export default App;
